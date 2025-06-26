8a5618802d18e7d9012dfa496321b774ff025ece
andrebarth <a.barth@escola.pr.gov.br>
2025-06-26 08:08:35 -0300

    kjk

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function CriaCartao(categoria, pergunta, resposta){
-let container = document.getElementById('container')
-let cartao = document.createElement('article')
-cartao.className = 'cartao'
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
- cartao.innerHTML = `
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
@@ -15,6 +15,15 @@ cartao.className = 'cartao'
     </div>
     `
 
-container.appendChild(cartao)
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
 
 }
